3a1b56cd660fc29fe35ab71c74f3c29e9222cabf
lucas.sousa <lucas.sousa@kasco.ai>
2024-12-22 17:34:53 -0300

    Corrigido código para que o sistema fique mais sensível à aproximação para sair do deep sleep

--- lib/DISTSensor/DISTSensor.cpp
+++ lib/DISTSensor/DISTSensor.cpp
@@ -1,67 +1,57 @@
 #include <DISTSensor.h>
 
-
+// Função para detectar algo se aproximando
 bool DISTSensor::measureClosing(){
-    uint8_t minDiff = 10;
-    VL53L0X_RangingMeasurementData_t measure;
-    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-    uint16_t lastMeasure = measure.RangeMilliMeter;
-    bool closing = 0;
-    for(int i = 0; i<=11; i++){
-        lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-        if(measure.RangeStatus == 4){
+    uint8_t minDiff = 10; // Velocidade com que a coisa precisa estar se aproximando (diferença entre a posição atual do objeto e da leitura anterior)
+    VL53L0X_RangingMeasurementData_t measure; // Variável que irá guardar os valores de distância
+    lox.rangingTest(&measure, false); // Obtendo leituras de distância (true salvaria também mensagens de debug)
+    uint16_t lastMeasure = measure.RangeMilliMeter; // Variável usada para comparar valores de distância
+    bool closing = 0; // Variável para identificar se há algo chegando
+    for(int i = 0; i<=11; i++){ // Efetuar 11 leituras para identificar se há algo se aproximando
+        lox.rangingTest(&measure, false); // Fazendo a leitura
+        if(measure.RangeStatus == 4){ // Caso o rangeStatus seja 4, houve algum problema na leitura
             continue;
         }
-        if(measure.RangeMilliMeter <= lastMeasure-minDiff){
-            if(closing == 0){
-                closing = 1;
-                lastMeasure = measure.RangeMilliMeter; 
+        if(measure.RangeMilliMeter <= lastMeasure-minDiff){ // Caso a medida atual seja menor ou igual à anterior - 10
+            if(closing == 0){ // Caso a variável closing seja 0, essa foi a primeira deteção
+                closing = 1; // Faz closing virar 1, identificando que já houve uma deteção
+                lastMeasure = measure.RangeMilliMeter; // Salva essa medida como a medida anterior par a próxima leitura
             }
             else{
                 return 1;
             }        
         }
+        lastMeasure = measure.RangeMilliMeter;
         delay(1);
     }
     return 0;
 }
 
 uint16_t DISTSensor::getDistance(){
-    Serial.println("Dentro do getDistance");
     VL53L0X_RangingMeasurementData_t measure;
-    Serial.println("Criou a variavel");
-    if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) { // PPor algum motivo, quando a função "mind" precisa chamar essa função getDistance, é preciso reinicializar o objeto do sensor de distância
-        Serial.println("Nada");
-        lox.begin();  // Reinitialize the sensor after wake up
-    }
-    lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-    Serial.println("Prestes a entrar no if"); 
-    if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-        Serial.println("Dentro do if do getDistance");
-        return measure.RangeMilliMeter;
+    // if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0) { // Por algum motivo, quando a função "mind" precisa chamar essa função getDistance, é preciso reinicializar o objeto do sensor de distância
+    lox.begin();  // Reiniciando o sensor por que POR ALGUM MOTIVO, na hora que a função "mind" chama a getDistance o sensor não está mais inicializado
+    // }
+    lox.rangingTest(&measure, false); // Leitura do sensor de distância
+    if (measure.RangeStatus != 4) {  // Caso não haja falha na leitura
+        return measure.RangeMilliMeter; // retorna a distância
     } else {
-        return 6000;
+        return 6000; // Caso haja falha, retorna um valor qualquer
     }
 }
 
 u_int16_t* DISTSensor::getDistances(){
-    return distances;
+    return distances; // Retorna um array com 3 distâncias
 }
 
-void DISTSensor::setDistances(u_int8_t pos){
-    if(pos < 3){
-        Serial.print("Dentro do if do setDistances. Posição: ");
-        Serial.println(pos);
-        distances[pos] = getDistance();
-        Serial.print("Distância salva: ");
-        Serial.println(distances[pos]);
-    }else{
-        Serial.println("bug");
+void DISTSensor::setDistances(u_int8_t pos){ // Função para setar um valor no array distances[]
+    if(pos < 3){ // Teste para ver se o índice selecionado está ok
+        distances[pos] = getDistance(); // Leitura da distância pelo sensor de distância e preenchimento do array
     }
 }
 
 void DISTSensor::begin(){
-    if(!lox.begin()){
+    if(!lox.begin()){ // Inicialização do sensor de distância
         while (true)
         {
             /* code */
--- lib/actuators/actuators.h
+++ lib/actuators/actuators.h
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 #include <DISTSensor.h>
 
-class Vision{
+class Vision{ // Classe do sensor de distância
     private:
         DISTSensor dist;
     public:
-    void eyeLeft(){
+    void eyeLeft(){ // Movimentar o sensor para a direita com um servo motor
         Serial.println("eyeLeft");
         delay(1500);
-        dist.setDistances(0);
+        dist.setDistances(0); // Setar o valor do índice 0 da variável distâncias
     }
-
+    // O mesmo se repete para right e center
     void eyeRight(){
         Serial.println("eyeRight");
         delay(1500);
@@ -23,55 +23,69 @@ class Vision{
         dist.setDistances(1);
     }
 
-    void reading(){
+    void reading(){ // Leitura de valores de distância à esqueda, centro e direita, preenchendo os índices do array
         eyeLeft();
         eyeCenter();
         eyeRight();
         eyeCenter();       
     }
 
-    u_int16_t* getDistances(){
+    u_int16_t* getDistances(){ // Obtenção do array distance[]
         return dist.getDistances();
     }
 
 };
 
-class Car{
+class Car{ // Classe de controle do carrinho
     private:
-    enum Direction: bool{left = false, right = true};
+    enum Direction: u_int8_t{left = 0, right = 1, straight = 2}; // Configurando valores a serem considerados para direções do carrinho usando um enum
     Vision vis;
-    Direction decideDirection(uint16_t* distances) {
-        int smallestIndex = 0;
+    Direction decideDirection(uint16_t* distances) { // Função que decide a direção que o carrinho irá se mover
+        int smallestIndex = 0; // Variável para comparação de valores do array
 
-        // Encontrar o menor valor no array
+        // Encontrar o menor valor no array de distâncias
         for (int i = 1; i < 3; i++) {
             if (distances[i] < distances[smallestIndex]) {
+                Serial.println("Distância lida:" + String(distances[i]));
                 smallestIndex = i;
             }
         }
 
         // Decidir para onde virar
-        if (distances[smallestIndex] <= 100) {
-            if (smallestIndex == 0) return right;
-            if (smallestIndex == 2) return left;
+        if (distances[smallestIndex] <= 500) {
+            Serial.println("A menor Distância lida:" + String(distances[smallestIndex]));
+            if (smallestIndex == 0) return right; // O valor lido quando o sensor está na esquerda (eyeLeft) é guardado no índice 0
+            if (smallestIndex == 2) return left; // O valor lido quando o sensor está na direita (eyeRight) é guardado no índice 2
         }
-        return left; // Padrão se não estiver perto de nada
+        return straight; // Padrão se não estiver perto de nada
     }
 
-    void turn(Direction dir){
-        if(dir == left){
+    void turn(Direction dir){ // Função para fazer o carrinho virar
+        if(dir == left){ 
             Serial.print("Virar à esquerda");
         }else{
             Serial.print("Virar à direita");
         }
     }
 
+    void forward(){ // Função para fazer o carrinho andar pra frente
+        Serial.println("Frente");
+    }
+
+    void backward(){ // Função para fazer o carrinho andar pra trás
+        Serial.println("Trás");
+    }
+
     public:
 
-    void mind(){
-        vis.reading();
-        u_int16_t* distances =  vis.getDistances();
-        Direction dir = decideDirection(distances);
-        turn(dir);
+    void mind(){ // Função que faz o carrinho agir
+        u_int16_t now = millis();
+        while (millis() - now < 10000){
+            vis.reading();
+            u_int16_t* distances =  vis.getDistances();
+            Direction dir = decideDirection(distances);
+            if(dir != straight) turn(dir);
+            else forward();
+        }        
     }
 };
