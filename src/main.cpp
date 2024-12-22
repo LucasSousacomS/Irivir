9e724bdc5d690bdd4213b98a0a0e2d174b4961fa
lucas.sousa <lucas.sousa@kasco.ai>
2024-12-22 17:35:42 -0300

    Corrigido bug que fazia o ESP sair do deep sleep sem o sensor PIR ser acionado e acionando o carrinho quando um objeto estiver próximo

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,21 @@ Car car;
 Vision vision;
 
 void setup() {
+  
   Serial.begin(9600);
   dist.begin();
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_4, 1);
   rtc_gpio_pullup_dis(GPIO_NUM_4);
   rtc_gpio_pulldown_en(GPIO_NUM_4);
+  Serial.println("Causa: " + String(esp_sleep_get_wakeup_cause()));
   // Reinitialize sensor if waking from deep sleep
   
-  if(dist.measureClosing()){
-    if(dist.getDistance() <= 100){
-      car.mind();
-    };
+  if((dist.measureClosing() && dist.getDistance() <= 300) || dist.getDistance() <= 100){
+    car.mind();
   }
+  delay(100);
   esp_deep_sleep_start();
 }
 
 void loop() {
-  Serial.println(dist.measureClosing());
 }
